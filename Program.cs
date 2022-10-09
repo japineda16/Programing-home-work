88d37c7ac895000304e0935d2d676345274e2d88
Jose Pineda <pinedaborges.josealejandro@gmail.com>
2022-10-09 16:04:07 -0400

    Suma de caracteres

--- Program.cs
+++ Program.cs
@@ -2,25 +2,28 @@
 {
     public class Program
     {
-        // Definiendo la variable de tipo string.
-        public static int? square;
-        public static int? area;
-        public static int? perimeter;
+        // Definiendo la variable de tipos long, string e int.
+        public static long? toSum;
+        public static string? sumString;
+        public static int total = 0;
         static void Main()
         {
             // Despliegue de log y obtencion de data.
             Console.WriteLine("Por favor indique el tamaño de uno de los lados del cuadrado.");
-            square = int.Parse(Console.ReadLine());
-            // Invoca la funcion, y esta devuelve tanto perimetro y superficie.
-            getAreaAndPerimeter(square);
-            // Respuesta del programa.
-            Console.WriteLine("El perimetro es " + perimeter);
-            Console.WriteLine("La superficie es " + area + "²");
+            toSum = long.Parse(Console.ReadLine());
+            // Invocamos la funcion que calcula la entrada por consola
+            operation();
+            Console.WriteLine("El total de " + toSum + " es " + total);
         }
         // Definicion de la funcion
-        static void getAreaAndPerimeter(int? info) {
-            perimeter = info * 4;
-            area = info * info;
+        static void operation() {
+            // Primero convertimos en string el parametro entrante para que lo podamos leer en un ciclo.
+            sumString = toSum.ToString();
+            // Creamos el ciclo para ir leyendo caracter por caracter e ir sumandolo uno a uno con la iteracion anterior.
+            for (int i = 0; i < sumString.Length; i++)
+            {
+                total = int.Parse(sumString.Substring(i,1)) + total;
+            }
         }
     }
 }
