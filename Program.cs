d863afbdf1c0d07b29124d37044f42aea3800f45
Jose Pineda <pinedaborges.josealejandro@gmail.com>
2022-10-09 15:26:08 -0400

    Final

--- Program.cs
+++ Program.cs
@@ -3,23 +3,24 @@
     public class Program
     {
         // Definiendo la variable de tipo string.
-        public static string? phrase;
-        public static string? final;
-        public static string? start;
+        public static int? square;
+        public static int? area;
+        public static int? perimeter;
         static void Main()
         {
             // Despliegue de log y obtencion de data.
-            Console.WriteLine("Escribe una sola palabra!");
-            phrase = Console.ReadLine();
-            // Invoca la funcion que devuelve el inicio y final de la palabra.
-            palabreo(phrase);
+            Console.WriteLine("Por favor indique el tamaño de uno de los lados del cuadrado.");
+            square = int.Parse(Console.ReadLine());
+            // Invoca la funcion, y esta devuelve tanto perimetro y superficie.
+            getAreaAndPerimeter(square);
             // Respuesta del programa.
-            Console.WriteLine("La primera letra es " + start + " y la ultima es " + final);
+            Console.WriteLine("El perimetro es " + perimeter);
+            Console.WriteLine("La superficie es " + area + "²");
         }
         // Definicion de la funcion
-        static void palabreo(string? palabra) {
-            final = phrase.Substring(phrase.Length - 1);
-            start = phrase.Substring(0,1);
+        static void getAreaAndPerimeter(int? info) {
+            perimeter = info * 4;
+            area = info * info;
         }
     }
 }
