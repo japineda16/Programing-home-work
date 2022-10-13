09147bb9489404fe1bc310edd1bab53346a6e155
Jose Pineda <pinedaborges.josealejandro@gmail.com>
2022-10-13 19:17:06 -0400

    ejercicio 7

--- Program.cs
+++ Program.cs
@@ -6,18 +6,25 @@ namespace App
     {
         static void Main()
         {
-            // Enseñamos en pantalla el siguiente mensaje:
-            Console.WriteLine("Introduce una palabra.");
-            // Solicitamos el ingreso del string a solicitar
-            string word = Console.ReadLine();
-            // Solicitamos el ingreso de la letra a buscar
-            Console.WriteLine("Introduce una letra a buscar.");
-            string character = Console.ReadLine();
-            // Contamos las letras y llevamos un registro de coincidencias de caracteres
-            // De tal manera que cada vez que "X" sea igual a "character", lleve el contador de
-            // coincidencias.
-            var count = word.Count(x => Char.ToString(x) == character);
-            Console.WriteLine("La palabra se repite " + count + " veces");
+            // Hacemos la peticion al usuario de los numeros a sacar el MCD
+            Console.WriteLine("Ingrese el primer numero.");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            // Buscamos el mayor y el menor de estos numeros para asignarlos a cada variable correspondiente
+            // Junto con una respuesta que por ahora estara vacia.
+            int max = Math.Max(number1, number2);
+            int min= Math.Min(number1, number2);
+            int response;
+            // Creamos las iteraciones a hasta llegar a 0 en el restante.
+            do
+            {
+                response = min; // Como resultado de cada iteracion guardamos el divisor.
+                min= max%min; // Guardamos el resto en b, para asi saber cuando acaba la iteracion.
+                max = response; // Pasa el diviendo al divisor.
+            } while (min!= 0);
+            Console.WriteLine("El M.C.D entre: " + number1 + " y " + number2 + " es " + response);
         }
     }
 }
